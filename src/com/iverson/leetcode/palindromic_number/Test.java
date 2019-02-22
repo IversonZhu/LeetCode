38f25d2c5d0ce018be567edd6b276df9622260da
Iverson <zhuhuayao_2014@163.com>
2019-02-23 00:16:44 +0800

    Initial commit

---
+++ src/com/iverson/leetcode/palindromic_number/Test.java
@@ -0,0 +1,7 @@
+package com.iverson.leetcode.palindromic_number;
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPalindrome(1236321));
+    }
+}
